d7fd50498ee38c5b00e8e91bda41d4765fbd92bd
Eduar Arias <eduar_arias95191@elpoli.edu.co>
2023-02-25 16:44:45 -0500

    Avance productos

---
+++ src/Products/Products.java
@@ -0,0 +1,85 @@
+package Products;
+
+import java.util.ArrayList;
+
+public class Products {
+
+    ArrayList<String> cards = new ArrayList<>();
+    ArrayList<String> saveAccount = new ArrayList<>();
+    ArrayList<String> currentAccount = new ArrayList<>();
+    public void cards(){
+        cards.add("""
+                Tipo de tarjeta
+                *Mastercard
+                
+                Caracteristicas
+                * Ingresos a partir de 1 millon
+                * cuota de manejo de $26.600
+                * Cupo entre 1 y 5 millones
+                * 6 meses sin cuota de manejo
+                
+                Beneficios
+                *Cobertura de viajes
+                *Ofertas y descuentos
+                *Pago sin contacto
+                *Avance del 100%
+                
+                politicas
+                * Intereses de 3.12% mensual y 45.16% anual
+                * Las compras a una cuota no generaran intereses
+                * Las compras interacionales se diferiran a 36 cuotas
+                * Los avances nacionales e internacionse difieren a 24 cuotas
+                """);
+        cards.add("""
+                Tipo de tarjeta
+                *Visa
+                
+                Caracteristicas
+                * Ingresos a partir de 1 millon
+                * cuota de manejo de $26.600
+                * Cupo entre 1 y 5 millones
+                * 6 meses sin cuota de manejo
+                
+                Beneficios
+                *Cobertura de viajes
+                *Ofertas y descuentos
+                *Pago sin contacto
+                *Desembolso de emergencia""\"
+                
+                politicas
+                * Intereses de 3.12% mensual y 45.16% anual
+                * Las compras a una cuota no generaran intereses
+                * Las compras interacionales se diferiran a 36 cuotas
+                * Los avances nacionales e internacionse difieren a 24 cuotas
+                """);
+        cards.add("""
+                Tipo de tarjeta
+                *American Express
+                
+                Caracteristicas
+                * Ingresos a partir de 1 millon
+                * cuota de manejo de $26.600
+                * Cupo entre 1 y 5 millones
+                * 6 meses sin cuota de manejo
+                
+                Beneficios
+                *2x1 en Cinemark
+                *Ofertas y descuentos
+                *Pago sin contacto
+                *Avance del 100%
+                
+                politicas
+                * Intereses de 3.12% mensual y 45.16% anual
+                * Las compras a una cuota no generaran intereses
+                * Las compras interacionales se diferiran a 36 cuotas
+                * Los avances nacionales e internacionse difieren a 24 cuotas
+                """);
+
+    }
+    public void saveAccount(){
+        saveAccount.add("""
+                
+               
+                """);
+    }
+}
